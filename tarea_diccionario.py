744e79a9d1e4b4e033505841e6f6ebb29bfa67e6
Xavier Abugannam <61886186+Knappenx@users.noreply.github.com>
2023-04-12 11:31:27 -0600

    Update tarea_diccionario.py

--- tarea_diccionario.py
+++ tarea_diccionario.py
@@ -9,21 +9,27 @@ diccionario={
         }
     }
 }
-contador=1
-while contador==1:
+
+extit_status = False
+
+while extit_status == False:
+    
     user_options=int(input("Selecione una opción:\nagregar usuario (1)\neditar usuario (2)\neliminar usuario(3)\nsalir (4)\n"))
+    
     if user_options==1:
         usuario=str(input("ingrese email de usuario: "))
         nombre=str(input("ingrese nombre: "))
         apellido=str(input("ingrese apellido: "))
         edad=int(input("ingrese edad: "))
-        antiguedad=int(0)
+        antiguedad=float(0)
         pregunta_subcripcion=str(input("¿El usuario pago la subscripcion? (Y/N) "))
         if pregunta_subcripcion=="Y":
             subscripcion=True
-        subscripcion=False
-        #revisar
+        else:
+            subscripcion=False
+        # TODO: Agregar logica para insertar datos al diccionario, se asignaron los valores a variables pero no se usan
         print(diccionario)
+        
     elif user_options==2:
         usuario=str(input("Ingrese el correo del usuario: "))
         if usuario in diccionario["usuarios"]:
@@ -31,15 +37,16 @@ while contador==1:
             diccionario["usuarios"][usuario]["nombre"]=str(input("ingrese nombre: "))
             diccionario["usuarios"][usuario]["apellido"]=str(input("ingrese apellido: "))
             diccionario["usuarios"][usuario]["edad"]=int(input("ingrese edad: "))
-            diccionario["usuarios"][usuario]["antiguedad"]=int(input("Ingese antiguedad: "))
+            diccionario["usuarios"][usuario]["antiguedad"]=float(input("Ingese antiguedad: "))
             pregunta_subcripcion=str(input("¿El usuario pago la subscripcion? (Y/N)"))
             if pregunta_subcripcion=="Y":
                 diccionario["usuarios"][usuario]["subscripcion"]=True
-            diccionario["usuarios"][usuario]["subscripcion"]=False
-            #revisar
+            else:
+                diccionario["usuarios"][usuario]["subscripcion"]=False
             print(diccionario)
         else:
             print("No se encontro al usuario")
+            
     elif user_options==3:
         usuario=str(input("Ingrese el correo del usuario: "))
         if usuario in diccionario["usuarios"]:
@@ -47,8 +54,10 @@ while contador==1:
             print("Usuario borrado...")
         else:
             print("No se encontro al usuario")
+            
     elif user_options==4:
         print("saliendo...")
-        contador=2
+        extit_status = True
+        
     else:
         print("Opcion invalida")
